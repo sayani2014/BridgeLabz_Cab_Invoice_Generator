699856eea253b43ba250db03a83ff75f7cbf2a9e
Sayani Koley <sayanikoley8@gmail.com>
2021-07-14 10:30:28 +0530

    Ability to return invoice given the userID and test it using JUnit testing

--- CabInvoiceGenerator/src/test/java/InvoiceServiceTest.java
+++ CabInvoiceGenerator/src/test/java/InvoiceServiceTest.java
@@ -7,6 +7,7 @@
  *       - Total Number of Rides
  *       - Total Fare
  *       - Average Fare Per Ride
+ * UC4 : Given userID, the Invoice Service gets the list of rides and returns the invoice.
  *
  * @author : SAYANI KOLEY
  * @since : 13.07.2021
@@ -15,6 +16,8 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Hashtable;
 
 public class InvoiceServiceTest {
     InvoiceGenerator invoiceGenerator = null;
@@ -85,5 +88,36 @@ public class InvoiceServiceTest {
         InvoiceSummary summary = invoiceGenerator.calculateFareSummary(rides);
         InvoiceSummary expectdInvoiceSummary = new InvoiceSummary(2, 30);
         Assert.assertEquals(expectdInvoiceSummary, summary);
+    }
+
+    /**
+     *** Step 4
+     * Purpose : Given userID,
+     *           calculate the aggregate total for all the rides
+     *           calculate the average fare per ride
+     *           return the final invoice for that particular userID
+     */
+
+    @Test
+    public void givenUserID_ShouldReturnInvoiceSummary() {
+        Hashtable<Integer, Ride[]> htable = new Hashtable<>();
+
+        int userID1 = 1;
+        Ride[] ride1 = { new Ride(2.0, 5),
+                new Ride(0.1, 1) };
+        htable.put(userID1, ride1);
+
+        int userID2 = 2;
+        Ride[] ride2 = { new Ride(4.0, 10),
+                new Ride(1, 1) };
+        htable.put(userID2, ride2);
+
+        int userID = 2;
+
+        if(htable.containsKey(userID)) {
+            InvoiceSummary summary = invoiceGenerator.calculateFareSummary(htable.get(userID));
+            InvoiceSummary expectdInvoiceSummary = new InvoiceSummary(2, 61);
+            Assert.assertEquals(expectdInvoiceSummary, summary);
+        }
     }
 }
