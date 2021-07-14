27f4f7179327ecdf1ab82ea25208fe37b245e538
Sayani Koley <sayanikoley8@gmail.com>
2021-07-14 10:25:14 +0530

    Ability to solve Enhanced Invoice and test it using JUnit testing

--- CabInvoiceGenerator/src/main/java/InvoiceGenerator.java
+++ CabInvoiceGenerator/src/main/java/InvoiceGenerator.java
@@ -41,4 +41,21 @@ public class InvoiceGenerator {
         }
         return totalFare;
     }
+
+    /**
+     * Purpose : Given distance and time for multiple rides,
+     *           Calculate aggregate total fare for all the journey
+     *           Calculate the average fare per ride taking total number of rides and total fare as input
+     *
+     * @param rides
+     * @return
+     */
+
+    public InvoiceSummary calculateFareSummary(Ride[] rides) {
+        double totalFare = 0.0;
+        for(Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
 }
--- CabInvoiceGenerator/src/test/java/InvoiceServiceTest.java
+++ CabInvoiceGenerator/src/test/java/InvoiceServiceTest.java
@@ -3,6 +3,10 @@
  *
  * UC1 : Given Distance and Time, the invoice generator should return the total fare for the journey.
  * UC2 : The Invoice Generator should take in multiple rides and calculate the total aggregate.
+ * UC3 : The Invoice Generator should return
+ *       - Total Number of Rides
+ *       - Total Fare
+ *       - Average Fare Per Ride
  *
  * @author : SAYANI KOLEY
  * @since : 13.07.2021
@@ -65,5 +69,21 @@ public class InvoiceServiceTest {
                 new Ride(0.1, 1) };
         double fare = invoiceGenerator.calculateTotalFare(rides);
         Assert.assertEquals(30, fare, 0.0);
+    }
+
+    /**
+     *** Step 3
+     * Purpose : Given distance and time,
+     *           calculate the aggregate total for all the rides
+     *           finally calculate the average fare per ride
+     */
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1) };
+        InvoiceSummary summary = invoiceGenerator.calculateFareSummary(rides);
+        InvoiceSummary expectdInvoiceSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectdInvoiceSummary, summary);
     }
 }
